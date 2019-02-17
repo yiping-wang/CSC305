23fc44544fb9f13a426921f11180fa300aa476b6
Yiping Wang <yipingwang@uvic.ca>
2019-02-16 21:26:47 -0800

    youtube

---
+++ Assignment 2 - YT/Assets/Editor/MapGeneratorEditor.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor (typeof (MapGenerator))]
+public class MapGeneratorEditor : Editor {
+
+    public override void OnInspectorGUI()
+    {
+        MapGenerator mapGen = (MapGenerator) target;
+        DrawDefaultInspector();
+        if (GUILayout.Button("Generate"))
+        {
+            mapGen.GenerateMap();
+        }
+    }
+}
