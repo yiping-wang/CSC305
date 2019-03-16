12fefbc6f34374814988105c5f31f4730b4dd876
Yiping Wang <yiping@wang.vision>
2019-03-15 20:41:07 -0700

    init assignment 3

---
+++ Assignment 3/Assets/Script/Flip.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flip : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Mesh mesh = gameObject.GetComponent<MeshFilter>().mesh;
+        int[] indices = mesh.triangles;
+        Vector3[] vertices = mesh.vertices;
+        Vector2[] UVs = mesh.uv;
+        for (int i = 0; i < vertices.Length; i++) 
+        {
+            vertices[i].y = vertices[i].y / 8f;
+        }
+        mesh.vertices = vertices;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
