c7c3cb09108b33c70c549fc33f943476a500e31f
Yiping Wang <yiping@wang.vision>
2019-03-25 21:46:15 -0700

    arc-length

--- Assignment 3/Assets/Script/CatmullRomFollower.cs
+++ Assignment 3/Assets/Script/CatmullRomFollower.cs
@@ -11,7 +11,8 @@ public class CatmullRomFollower : MonoBehaviour {
     float speed;
     bool coroutineAllowed;
     int controlPointsIndex;
-
+    float distance_to_travel = 0.01f;
+    float very_small_amount = 0.05f;
 	void Start () 
     {
         t = 0;
@@ -42,11 +43,21 @@ public class CatmullRomFollower : MonoBehaviour {
         Vector3 p2 = controlPointsList[CatmullRomSpline.ClampListPos(controlPointsIndex + 1, controlPointsList)].position;
         Vector3 p3 = controlPointsList[CatmullRomSpline.ClampListPos(controlPointsIndex + 2, controlPointsList)].position;
         controlPointsIndex += 1;
+        float travelled = 0;
 
         while (t < 1)
         {
             t += Time.deltaTime * speed;
             pos = CatmullRomSpline.GetCatmullRomPosition(t, p0, p1, p2, p3);
+
+            // Numerical Methods for Arc Length
+            while (travelled < distance_to_travel)
+            {
+                t += very_small_amount;
+                travelled += (CatmullRomSpline.GetCatmullRomPosition(t, p0, p1, p2, p3) - pos).magnitude;
+                pos = CatmullRomSpline.GetCatmullRomPosition(t, p0, p1, p2, p3);
+            }
+
             Vector3 newDir = Vector3.RotateTowards(transform.forward, (pos - transform.position).normalized, Time.deltaTime * speed * 10, 0.0f);
             transform.rotation = Quaternion.LookRotation(newDir);
             transform.position = pos;
